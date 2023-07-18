4a4c9efbea307e512d2edb323af70cd47b724beb
J-Chan96 <juchan1996@naver.com>
2023-07-18 12:06:45 +0900

    header design

--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -4,9 +4,10 @@ import styled from 'styled-components';
 const Header = styled.header`
   width: 100%;
   background: #333;
-  color: #fff;
+  color: rgba(255, 255, 255, 0.95);
+  font-weight: bold;
   display: flex;
-  /* justify-content: space-around; */
+  justify-content: space-around;
   padding: 20px;
   background-color: rgb(18, 22, 30);
   box-shadow:
@@ -19,7 +20,7 @@ const Header = styled.header`
 `;
 
 const Logo = styled.div`
-  padding-top: 8px;
+  /* padding-top: 8px; */
 `;
 
 const Menu = styled.nav`
@@ -27,10 +28,10 @@ const Menu = styled.nav`
 `;
 
 const MenuItem = styled.div`
-  margin: 0 10px;
+  margin: 0px 25px;
   position: relative;
   transition: color 0.3s ease-in-out;
-  padding: 10px 0;
+  align-items: center;
 
   &:hover {
     color: #f9a109;
@@ -47,20 +48,20 @@ const Dropdown = styled.div`
   position: absolute;
   background: #fff;
   color: #333;
-  padding: 10px;
-  margin-top: 20px;
+  /* padding: 15px;
+  margin-top: 20px; */
   border-radius: 8px;
   box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
   transition:
     opacity 0.3s ease-in-out,
     transform 0.3s ease-in-out;
   transform: translateY(-10px);
-  opacity: 0;
+  /* opacity: 0; */
 
-  &:hover {
+  /* &:hover {
     opacity: 1;
     transform: translateY(0);
-  }
+  } */
 `;
 
 function AppHeader() {
@@ -69,42 +70,28 @@ function AppHeader() {
       <Logo>Logo</Logo>
       <Menu>
         <MenuItem>
-          Menu 1
+          Learn
           <Dropdown>
             Submenu 1<br />
             Submenu 2
           </Dropdown>
         </MenuItem>
         <MenuItem>
-          Menu 2
+          Build
           <Dropdown>
             Submenu 1<br />
             Submenu 2
           </Dropdown>
         </MenuItem>
         <MenuItem>
-          Menu 3
+          Network
           <Dropdown>
             Submenu 1<br />
             Submenu 2
           </Dropdown>
         </MenuItem>
         <MenuItem>
-          Menu 4
-          <Dropdown>
-            Submenu 1<br />
-            Submenu 2
-          </Dropdown>
-        </MenuItem>
-        <MenuItem>
-          Menu 5
-          <Dropdown>
-            Submenu 1<br />
-            Submenu 2
-          </Dropdown>
-        </MenuItem>
-        <MenuItem>
-          Menu 6
+          Community
           <Dropdown>
             Submenu 1<br />
             Submenu 2
