d0b6b3acbc705ce73d1685daf6783167ac6da517
J-Chan96 <juchan1996@naver.com>
2023-07-18 10:25:04 +0900

    font update

--- src/App.js
+++ src/App.js
@@ -1,21 +1,31 @@
 import React, { useEffect } from 'react';
 import AppLayout from './components/layout/AppLayout';
-import styled from 'styled-components';
+import styled, { createGlobalStyle } from 'styled-components';
 
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+`;
 
-  @media (min-width: 768px) {
-    flex-direction: row;
+const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: 'WLFont';
+    src: url('/public/fonts/Montserrat-Regular.ttf') format('truetype');
+  }
+
+  body {
+    font-family: 'WLFont', sans-serif;
   }
 `;
 
 function App() {
   return (
-    <Container>
-      <AppLayout />
-    </Container>
+    <>
+      <GlobalStyles />
+      <Container>
+        <AppLayout />
+      </Container>
+    </>
   );
 }
 
--- src/components/layout/AppHeader.jsx
+++ src/components/layout/AppHeader.jsx
@@ -6,8 +6,20 @@ const Header = styled.header`
   background: #333;
   color: #fff;
   display: flex;
-  justify-content: space-between;
+  /* justify-content: space-around; */
   padding: 20px;
+  background-color: rgb(18, 22, 30);
+  box-shadow:
+    rgba(0, 0, 0, 0.15) 0px 2px 6px 2px,
+    rgba(0, 0, 0, 0.3) 0px 1px 2px;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+const Logo = styled.div`
+  padding-top: 8px;
 `;
 
 const Menu = styled.nav`
@@ -17,6 +29,13 @@ const Menu = styled.nav`
 const MenuItem = styled.div`
   margin: 0 10px;
   position: relative;
+  transition: color 0.3s ease-in-out;
+  padding: 10px 0;
+
+  &:hover {
+    color: #f9a109;
+    cursor: pointer;
+  }
 
   &:hover div {
     display: block;
@@ -29,13 +48,25 @@ const Dropdown = styled.div`
   background: #fff;
   color: #333;
   padding: 10px;
-  margin-top: 10px;
+  margin-top: 20px;
+  border-radius: 8px;
+  box-shadow: 0 3px 10px rgba(0, 0, 0, 0.2);
+  transition:
+    opacity 0.3s ease-in-out,
+    transform 0.3s ease-in-out;
+  transform: translateY(-10px);
+  opacity: 0;
+
+  &:hover {
+    opacity: 1;
+    transform: translateY(0);
+  }
 `;
 
 function AppHeader() {
   return (
     <Header>
-      <div>Logo</div>
+      <Logo>Logo</Logo>
       <Menu>
         <MenuItem>
           Menu 1
@@ -53,6 +84,27 @@ function AppHeader() {
         </MenuItem>
         <MenuItem>
           Menu 3
+          <Dropdown>
+            Submenu 1<br />
+            Submenu 2
+          </Dropdown>
+        </MenuItem>
+        <MenuItem>
+          Menu 4
+          <Dropdown>
+            Submenu 1<br />
+            Submenu 2
+          </Dropdown>
+        </MenuItem>
+        <MenuItem>
+          Menu 5
+          <Dropdown>
+            Submenu 1<br />
+            Submenu 2
+          </Dropdown>
+        </MenuItem>
+        <MenuItem>
+          Menu 6
           <Dropdown>
             Submenu 1<br />
             Submenu 2
