e82e4b8a2a0dab04bef08c0a293615296bec708f
LiberVance <libervance@LiberVanceui-MacBookPro-2.local>
2023-07-19 17:57:31 +0900

    폴더정리

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import MainDashboard from '../components/main/MainDashboard';
+import MainStatistics from '../components/main/MainStatistics';
+
+function Main() {
+  return (
+    <>
+      <MainDashboard />
+      <MainStatistics />
+    </>
+  );
+}
+
+export default Main;
