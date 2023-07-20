d0fdfbb09efd82cb9dd5a0b81c6b445d38842ac8
J-Chan96 <juchan1996@naver.com>
2023-07-20 17:18:46 +0900

    MainData deploy

--- src/components/main/MainData.tsx
+++ src/components/main/MainData.tsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-import Img from '../../assets/images/3dicons.png';
 
 const ContainerData = styled.div`
   width: 100%;
@@ -14,47 +13,81 @@ const Head = styled.div`
   font-weight: bold;
   font-size: calc(20px + 2vmin);
   margin: 0 auto;
+  padding-bottom: 30px;
 `;
 
 const Section = styled.div`
+  width: 80%;
   display: flex;
-  justify-content: row;
-`;
-
-const Image = styled.img`
-  width: 300px;
-  height: 300px;
+  /* align-items: center; */
+  /* margin-left: 110px; */
+  margin: 0 auto;
+  gap: 32px;
+  /* align-self: stretch; */
 `;
 
 const Content = styled.div`
-  width: 100%;
   display: flex;
   flex-direction: column;
   justify-content: center;
+  align-items: flex-start;
+  gap: 16px;
+  flex: 1 0 0;
+`;
+
+const Body1 = styled.div`
+  border-color: #1fcff1;
+  border-left: 2px solid;
+  padding-left: 15px;
+  font-size: 1.75rem;
+  font-weight: 700;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+`;
+const Body2 = styled.div`
+  border-color: rgb(31, 207, 241);
+  border-left: 2px solid;
+  padding-left: 15px;
+  font-size: 1.75rem;
+  font-weight: 700;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+`;
+const Body3 = styled.div`
+  border-color: rgb(31, 207, 241);
+  border-left: 2px solid;
+  padding-left: 15px;
+  font-size: 1.75rem;
+  font-weight: 700;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
+`;
+const Body4 = styled.div`
+  border-color: rgb(31, 207, 241);
+  border-left: 2px solid;
+  padding-left: 15px;
+  font-size: 1.75rem;
+  font-weight: 700;
+  margin-top: 0;
+  margin-bottom: 0.5rem;
 `;
 
 function MainData() {
   return (
     <ContainerData>
-      <Head>There are WorldLand Technology</Head>
+      <Head>Core Features</Head>
       <Section>
         <Content>
-          <div>
-            <Image src={Img} alt="3d"></Image>
-          </div>
-          <div>BlockChain 3D IMG</div>
+          <Body1>EVM Compatibility</Body1>
         </Content>
         <Content>
-          <div></div>
-          <div></div>
+          <Body2>PQ Security</Body2>
         </Content>
         <Content>
-          <div></div>
-          <div></div>
+          <Body3>Energy Efficiency</Body3>
         </Content>
         <Content>
-          <div></div>
-          <div></div>
+          <Body4>Decentralized</Body4>
         </Content>
       </Section>
     </ContainerData>
