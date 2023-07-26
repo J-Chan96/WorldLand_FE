cec55c797fa532ffa2c203b6dda47897042d3e3f
parkgadan <parkgadani@gmail.com>
2023-07-26 15:55:07 +0900

    fix console error of learn page

--- src/components/Learn/LearnNav/index.style.tsx
+++ src/components/Learn/LearnNav/index.style.tsx
@@ -1,5 +1,5 @@
 import { styled } from 'styled-components';
-import { Link, LinkProps } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { maxQuery } from 'utils/breakpoints';
 
 interface ContainerProps {
