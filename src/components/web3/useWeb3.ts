18ef75a25a8ed78eb76ee27a1e4f7c834586a39f
LiberVance <libervance@LiberVanceui-MacBookPro-2.local>
2023-07-27 16:40:29 +0900

    fix_statistics_data

--- src/components/web3/useWeb3.ts
+++ src/components/web3/useWeb3.ts
@@ -1,7 +1,8 @@
 import Web3 from 'web3';
 
-// const infuraurl = 'http://43.200.52.189:8545';
-const infuraurl = 'https://rpc.lvscan.io';
+const infuraurl = 'https://gwangju.worldland.foundation/';
+// const infuraurl = 'https://seoul.worldland.foundation/';
+// const infuraurl = 'https://rpc.lvscan.io';
 const provider = new Web3.providers.HttpProvider(infuraurl);
 const web3 = new Web3(provider);
 
