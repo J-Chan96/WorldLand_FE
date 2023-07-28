dd0a0f457210895b50e3e61bca738bb854f69b23
Saddam-tech <salokhiddinov0727@gmail.com>
2023-07-28 10:40:24 +0900

    working on swap\ input/output component

---
+++ src/components/SwapInputTab/index.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+const SwapInputTab = () => {
+    return (
+        <Container>
+            <div>
+                {/* Swap */}
+                {/* Buy     Setting */}
+            </div>
+            <div>
+                <input type="text" />
+                <input type="text" />
+            </div>
+            <button>Swap / Connect Wallet</button>
+        </Container>
+    )
+}
+
+export default SwapInputTab
+
+const Container = styled.div`
+
+`
