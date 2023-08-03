6c970d8d2513d4801045760b522fb8bdb957e40d
Saddam-tech <salokhiddinov0727@gmail.com>
2023-08-03 16:05:33 +0900

    working on the config setup

---
+++ src/configs/services/chainIds.ts
@@ -0,0 +1,6 @@
+import { chainids } from 'types/main';
+
+export const chainIds: chainids = {
+  sepolia: 11155111,
+  worldland: 12345,
+};
--- src/types/main.d.ts
+++ src/types/main.d.ts
@@ -1,3 +1,11 @@
 interface SwapInputType {
   input: string;
 }
+
+export type ADDRESS_TYPE = {
+  [key: string]: `0x${string}` | any;
+};
+
+export type chainids = {
+  [key: string]: number;
+};
