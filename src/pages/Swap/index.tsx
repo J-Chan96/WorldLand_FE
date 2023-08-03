d16b7127fd941c86ce72b473822e8000a6dd539b
Saddam-tech <salokhiddinov0727@gmail.com>
2023-08-03 19:13:12 +0900

    config folder update

--- src/pages/Swap/index.tsx
+++ src/pages/Swap/index.tsx
@@ -1,7 +1,101 @@
+import { useState } from "react";
 import SwapInputTab from "components/SwapInputTab"
 import { styled } from "styled-components"
+import { chainIds } from "configs/services/chainIds";
+import { useAccount, useNetwork, useSwitchNetwork, useBalance, useContractWrite, useWaitForTransaction, useContractRead } from "wagmi";
+import { MAP_STR_ABI } from "configs/abis";
+import { wld_addresses } from "configs/contract_addresses";
 
 const Swap = () => {
+    const { address, isConnected } = useAccount();
+    const [amount, setAmount] = useState<string>("");
+    const [currentTxHash, setCurrentTxHash] = useState<`0x${string}` | undefined>();
+
+    const { chain } = useNetwork()
+
+    const { chains, switchNetwork } =
+        useSwitchNetwork({
+            onSuccess(data) {
+                //
+            },
+        })
+
+
+    const { data } = useWaitForTransaction({
+        chainId: chain?.id,
+        hash: currentTxHash,
+        staleTime: 2_000,
+        onSuccess() {
+            //
+        },
+        onError(error: any) {
+            //
+        },
+    })
+
+    const { data: allowance, isError, isLoading } = useContractRead({
+        // chainId: chain?.id,
+        // address: wld_addresses["token_contract"],
+        // abi: MAP_STR_ABI["ERC20_ABI"],
+        // functionName: 'allowance',
+        // args: [
+        //   address,
+        //   wld_addresses["bridge_contract"],
+        // ],
+    })
+
+    const { write } = useContractWrite({
+        chainId: chain?.id,
+        address: wld_addresses["wrappedETH_address_BRIDGE"],
+        abi: MAP_STR_ABI["WRAPETH"],
+        functionName: 'burn',
+        onMutate({ args }) {
+            if (!isConnected) {
+                // handleOpenNotification({
+                //     status: "Please connect your wallet!",
+                // });
+                return;
+            }
+            if (chain?.id !== chainIds["worldland"]) {
+                switchNetwork?.(chainIds["worldland"])
+            }
+
+            if (amount === "") {
+                return;
+            }
+
+            //   setLoader(true);
+            //   handleOpenNotification({
+            //     status: "Sending transaction...",
+            //   });
+        },
+        onSuccess({ hash }) {
+            //   handleOpenNotification({
+            //     status: "Awaiting the transaction...",
+            //   });
+            setCurrentTxHash(hash);
+        },
+        onError(error: any) {
+            alert(error)
+            console.log(error);
+            //   handleOpenNotification({
+            //     status: error?.shortMessage,
+            //   });
+        }
+    })
+
+
+    const sendTransaction = async () => {
+        try {
+            write({
+                args: [to_wei(amount)],
+            })
+        } catch (err) {
+            console.log(err)
+        }
+    };
+
+
     return (
         <Container>
             <SwapInputTab input="1.0" />
---
+++ src/utils/util.ts
@@ -0,0 +1,22 @@
+export const from_wei = (val) => Web3.utils.fromWei('' + val, 'ether');
+export const to_wei = (val) => Web3.utils.toWei('' + val, 'ether');
+
+export function putCommaAtPrice(data: number | string, precision: number | string) {
+  let str;
+
+  if (data !== undefined) {
+    data = Number(data);
+
+    if (Number.isFinite(+precision)) {
+      data = data.toFixed(+precision);
+    }
+    // if (data < 1000)
+    //   return data.toFixed(3);
+
+    str = data.toString().split('.');
+
+    str[0] = `${str[0]}`.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+    return str.join('.');
+  }
+  return 0;
+}
