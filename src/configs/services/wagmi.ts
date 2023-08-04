af26490e855773a522fd241af41f031b5470501c
Saddam-tech <salokhiddinov0727@gmail.com>
2023-08-04 10:49:17 +0900

    improving UI

--- src/configs/services/wagmi.ts
+++ src/configs/services/wagmi.ts
@@ -1,24 +1,24 @@
-// import { Chain } from 'wagmi';
+import { Chain } from 'wagmi';
 
-// export const worldland = {
-//   id: 12345,
-//   name: 'Worldland',
-//   network: 'worldland',
-//   nativeCurrency: {
-//     decimals: 18,
-//     name: 'EETH',
-//     symbol: 'EETH',
-//   },
-//   rpcUrls: {
-//     public: { http: ['https://rpc.lvscan.io'] },
-//     default: { http: ['https://rpc.lvscan.io'] },
-//   },
-//   blockExplorers: {
-//     etherscan: { name: 'lvscan', url: 'https://lvscan.io' },
-//     default: { name: 'lvscan', url: 'https://lvscan.io' },
-//   },
-// } as const satisfies Chain;
+export const worldland = {
+  id: 12345,
+  name: 'Worldland',
+  network: 'worldland',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'EETH',
+    symbol: 'EETH',
+  },
+  rpcUrls: {
+    public: { http: ['https://rpc.lvscan.io'] },
+    default: { http: ['https://rpc.lvscan.io'] },
+  },
+  blockExplorers: {
+    etherscan: { name: 'lvscan', url: 'https://lvscan.io' },
+    default: { name: 'lvscan', url: 'https://lvscan.io' },
+  },
+} as const satisfies Chain;
 
-// export const chainImages = {
-//   12345: 'https://lv-storage1.s3.amazonaws.com/LiberVance_Logo.svg',
-// };
+export const chainImages = {
+  12345: 'https://lv-storage1.s3.amazonaws.com/LiberVance_Logo.svg',
+};
